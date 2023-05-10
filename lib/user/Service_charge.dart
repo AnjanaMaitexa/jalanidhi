62eb004d867ba81374f2885141d43ef4e45e22a9
MaiTexa <maitexaitsolutions@gmail.com>
2023-05-10 06:35:33 +0530

    Second

--- lib/user/Service_charge.dart
+++ lib/user/Service_charge.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:jalanidhi/meter_reader/Meter_reading.dart';
 import 'package:jalanidhi/user/Home%20page.dart';
+import 'package:jalanidhi/user/api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'View_meter_reading.dart';
 
 class Charge extends StatefulWidget {
-  const Charge ({Key? key}) : super(key: key);
+ late final int id;
+ late final String rs;
+ Charge({required this.id,required this.rs});
 
   @override
   State<Charge> createState() => _ChargeState();
@@ -16,6 +23,54 @@ enum Gender { credit_card,debit_card,net_banking }
   String? payment;
 
 class _ChargeState extends State<Charge> {
+
+  late SharedPreferences localStorage;
+  late int user_id;
+  TextEditingController payment_typecontroller=TextEditingController();
+  TextEditingController amountcontroller=TextEditingController();
+  TextEditingController connection_datecontroller=TextEditingController();
+  bool _isLoading=false;
+  void registerUser()async {
+    localStorage = await SharedPreferences.getInstance();
+    user_id = (localStorage.getInt('user_id') ?? 0);
+    amountcontroller.text=widget.rs;
+    print(amountcontroller.text);
+    setState(() {
+      _isLoading = true;
+    });
+
+    var data = {
+      "consumer": user_id.toString(),
+      "reading_amount": amountcontroller.text,
+    };
+    print(data);
+    var res = await Api().authData(data,'/api/meter_reading_payment');
+    var body = json.decode(res.body);
+    print(body);
+    if(body['success']==true)
+    {
+      print(body);
+
+      _showDialog(context);
+      Fluttertoast.showToast(
+        msg: body['message'].toString(),
+        backgroundColor: Colors.grey,
+      );
+
+      _showDialog(context);
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>userhome()));
+
+    }
+    else
+    {
+      Fluttertoast.showToast(
+        msg: body['message'].toString(),
+        backgroundColor: Colors.grey,
+      );
+
+    }
+  }
+
   Future<void> _showDialog(BuildContext context) {
     return showDialog(
         context: context,
@@ -23,7 +78,17 @@ class _ChargeState extends State<Charge> {
           return AlertDialog(
             title: const Text("payment successfull"),
             content: Image.asset("images/img_11.png"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () =>  Navigator.push(context, MaterialPageRoute(builder: (context)=>userhome())),
 
+          child: const Text('OK'),
+              ),
+            ],
           );
         });
   }
@@ -95,9 +160,12 @@ class _ChargeState extends State<Charge> {
                 ),
               ),
               SizedBox(height: 30),
-              TextField(decoration: InputDecoration(
-                labelText:"Charge" ,
-                hintText: "charge",
+              TextField(
+                controller: amountcontroller,
+                readOnly: true,
+                decoration: InputDecoration(
+                labelText:widget.rs ,
+                hintText: widget.rs,
                 hintStyle: TextStyle(
                     color: Colors.green
                 ),
@@ -129,7 +197,7 @@ class _ChargeState extends State<Charge> {
                       // padding: EdgeInsets.all(20)
                     ),
                     onPressed: (){
-                      _showDialog(context);
+                      registerUser();
                     } ,
                     child: Text("CONTINUE")),
               ),
--- lib/user/View_meter_reading.dart
+++ lib/user/View_meter_reading.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:jalanidhi/user/Home%20page.dart';
 import 'package:jalanidhi/meter_reader/Meter_reader_home.dart';
 import 'package:jalanidhi/user/Service_charge.dart';
 import 'package:jalanidhi/user/api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 class Viewreading extends StatefulWidget {
   const Viewreading({Key? key}) : super(key: key);
 
@@ -16,21 +18,22 @@ class Viewreading extends StatefulWidget {
 
 class _ViewreadingState extends State<Viewreading> {
   TextEditingController readController = TextEditingController();
-  TextEditingController  payController = TextEditingController();
-  TextEditingController  dueController = TextEditingController();
-
+  TextEditingController payController = TextEditingController();
+  TextEditingController dueController = TextEditingController();
+  List _loadnoti = [];
   late SharedPreferences prefs;
   late int user_id;
-    String reading='';
-   String payment='';
-  String date='';
+  late int pay_id;
+  late int reading;
+  String payment = '';
+  String date = '';
   @override
   initState() {
     super.initState();
-    _viewPro();
+    _fetchArea();
   }
 
-  Future<void> _viewPro() async {
+  /* Future<void> _viewPro() async {
     prefs = await SharedPreferences.getInstance();
     user_id = (prefs.getInt('user_id') ?? 0 );
     print('login_idupdate ${user_id}');
@@ -43,26 +46,131 @@ class _ViewreadingState extends State<Viewreading> {
       print(reading);
       payment = body['data']['meter_reading_price'];
       date = body['data']['meter_reading_duedate'];
-      readController.text = reading;
+      readController.text = reading.toString();
       payController.text = payment;
       dueController.text = date;
 
     });
+  }*/
+  _fetchArea() async {
+    prefs = await SharedPreferences.getInstance();
+    user_id = (prefs.getInt('user_id') ?? 0);
+    print('login_idupdate ${user_id}');
+
+    var res =
+        await Api().getData('/api/consumer_reading/' + user_id.toString());
+    if (res.statusCode == 200) {
+      var items = json.decode(res.body)['data'];
+      print("items${items}");
+      setState(() {
+        _loadnoti = items;
+      });
+    } else {
+      setState(() {
+        _loadnoti = [];
+        Fluttertoast.showToast(
+          msg: "Currently there is no data available",
+          backgroundColor: Colors.grey,
+        );
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController datecontroller=TextEditingController();
+    TextEditingController datecontroller = TextEditingController();
     return MaterialApp(
-        home: Scaffold( appBar: AppBar(
-    title: Text("VIEW_METERREADING"),
-    leading:IconButton(
-    icon:Icon(Icons.arrow_back), onPressed: () {
-      Navigator.push(context,MaterialPageRoute(builder: (context)=>userhome()));
-    },
-    ),
-    ),
-    body: Container(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("VIEW_METERREADING"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => userhome()));
+          },
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: _loadnoti.length,
+          itemBuilder: (BuildContext context, int index) {
+            pay_id=   _loadnoti[index]['id'];
+            String rs= _loadnoti[index]['meter_reading_price'];
+            return Card(
+              color: Colors.blueGrey,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: ListTile(
+                  // leading: Image(
+                  //   image: AssetImage(
+                  //       Letter[index]
+                  //   ),
+                  //
+                  // )
+                  // ,
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Reading" + _loadnoti[index]['current_meter_reading'],
+                        style:
+                            TextStyle(fontSize: 20, color: Colors.yellowAccent),
+                      ),
+                      Text(
+                        "Due Date" + _loadnoti[index]['meter_reading_duedate'],
+                        style:
+                            TextStyle(fontSize: 20, color: Colors.yellowAccent),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text(
+                    "₹" + _loadnoti[index]['meter_reading_price'],
+                    style: TextStyle(fontSize: 20, color: Colors.yellowAccent),
+                  ),
+                  trailing: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        // padding: EdgeInsets.all(20)
+                      ),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Charge(id:pay_id,rs:_loadnoti[index]['meter_reading_price'],)));
+                      },
+                      child: Text("PAYMENT")),
+                ), /*,,
+                  trailing:
+                  ElevatedButton(
+
+                      style: ElevatedButton.styleFrom(
+                          primary:Colors.grey,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                            side: BorderSide(strokeAlign: BorderSide.strokeAlignCenter),
+                          ),
+                          padding: EdgeInsets.all(0)
+
+                      ),
+
+                      onPressed:(){},
+                      child: Text("CANCEL",
+                        style: TextStyle(
+                            color: Colors.black
+                        ),
+                      )
+                  ),*/
+              ),
+              // IconButton(
+              //   icon: Icon(
+              //       Icons.ca), onPressed: (
+              //     ){},
+              // ),
+            );
+          }),
+
+      /* Container(
     // ignore: prefer_const_literals_to_create_immutables
     child: Column(
     mainAxisAlignment: MainAxisAlignment.center,
@@ -137,8 +245,7 @@ class _ViewreadingState extends State<Viewreading> {
     )
         ]
     )
-    )
-    )
-    );
+    )*/
+    ));
   }
 }
